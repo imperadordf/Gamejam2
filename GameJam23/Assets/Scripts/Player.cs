3cf7c43202a23146fe55965f152d1ef709e8eca5
imperadordf <imperadordf@users.noreply.github.com>
2020-12-28 00:48:17 -0300

    Cena Teste + Tentando Mudar Gravidade

--- GameJam23/Assets/Scripts/Player.cs
+++ GameJam23/Assets/Scripts/Player.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Player : MonoBehaviour
 {
+    bool gravidade;
+    public Rigidbody rigi;
+   public CinemachineFreeLook cinema;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MudarGravidade();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MudarGravidade();
+        }
+    }
+
+    private void MudarGravidade()
+    {
+        gravidade = !gravidade;
+        if (gravidade)
+        {
+            Physics.gravity = new Vector3(0, -20, 0);
+            transform.localRotation = new Quaternion(0, 0, 0,0);
+            cinema.m_Lens.Dutch = 0;
+        }
+        else
+        {
+            Physics.gravity = new Vector3(0, 20, 0);
+            transform.localRotation = new Quaternion(0, 0, 180, 0);
+            cinema.m_Lens.Dutch = 180;
+        }
     }
 }
